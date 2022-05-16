9aded9160d942c3cffb45974fa11df3f0a694a1d
AlekseyKhaleev <aleksey52nn@gmail.com>
2022-05-16 23:43:43 +0300

    All code commeted

--- table_func.h
+++ table_func.h
@@ -1,5 +1,10 @@
+/**********************************************************************************************************************
+* Данный модуль содержит определения функций, предназначенных для работы с таблицей (создание, инициализация, вывод)
+***********************************************************************************************************************/
+
 #pragma once
 
+/* Подключение модулей*/
 #include <array>
 #include <fstream>
 
@@ -9,10 +14,14 @@ void NewError();
 /* Считывание кода нажатой клавиши в терминале без промежуточной буферизации */
 int UserKey();
 
-int CreateTable(char *argv[], std::fstream &table_out, int rows_request);
-
+/* Печать указанного количества строк таблицы, начиная с указанной строки */
 void PrintTable(struct PC ParkContent[], int rows_request, int row_number);
 
+/* Запись в бинарный файл данных таблицы, введенных пользователем */
+int CreateTable(char *argv[], std::fstream &table_out, int rows_request);
+
+/* Просмотр таблицы, с поддержкой управляющих клавиш */
 void ViewTable(char *argv[], std::fstream &table_in, int rows_request, int row_number=0);
 
+/* Вывод страницы с таблицей */
 void GenerateTablePage(struct PC ParkContent[], char* argv[], int rows_to_print, int real_rows, int row_number);
